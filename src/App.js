7785be47e00f98f5f28de4d6013390d41ccebd19
Zoms <martinezoms15@gmail.com>
2022-03-27 19:58:11 +0100

    project ready

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { apple, cart, google, habitual, home, main, orders, product, profile } from "./images";
+import "./App.css";
 
 function App() {
+  const { body } = document;
+
+  const changeBackground = (number) => {
+    // Check if background is already showing
+    let prevBg;
+    if (body.className) {
+      prevBg = body.className;
+    }
+    body.className = "";
+    switch (number) {
+      case "1":
+        return prevBg === "bg-1" ? false : body.classList.add("bg-1");
+      case "2":
+        return prevBg === "bg-2" ? false : body.classList.add("bg-2");
+      case "3":
+        return prevBg === "bg-3" ? false : body.classList.add("bg-3");
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="logo">
+        <img src={habitual} alt="logo" />
+      </div>
+      <div className="background-toggles" title="Change Background">
+        <div className="bg-1" onClick={() => changeBackground("1")}></div>
+        <div className="bg-2" onClick={() => changeBackground("2")}></div>
+        <div className="bg-3" onClick={() => changeBackground("3")}></div>
+      </div>
+      <div className="wrapper">
+        <div className="text-container">
+          <h1 className="title">Find the stuff you’ll love.</h1>
+          <p className="text">
+            Aliquam rhoncus pretium dolor volutpat lectus odio non tellus neque. Aliquet et purus nibh bibendum integer
+            etiam in enim nisi.
+          </p>
+          <div className="stores">
+            <div className="store">
+              <a href="https://apple.com" target="_blank" rel="noreferrer">
+                <img src={apple} alt="apple store" />
+              </a>
+            </div>
+            <div className="store">
+              <a href="https://play.google.com" target="_blank" rel="noreferrer">
+                <img src={google} alt="google store" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="image">
+          <img src={main} alt="" />
+        </div>
+      </div>
+
+      <div className="cart">
+        <img src={cart} alt="cart" />
+      </div>
+      <div className="home">
+        <img src={home} alt="home" />
+      </div>
+      <div className="orders">
+        <img src={orders} alt="orders" />
+      </div>
+      <div className="product">
+        <img src={product} alt="product" />
+      </div>
+      <div className="profile">
+        <img src={profile} alt="profile" />
+      </div>
     </div>
   );
 }
---
+++ src/images.js
@@ -0,0 +1,9 @@
+export { default as apple } from "./assets/apple.png";
+export { default as cart } from "./assets/cart.png";
+export { default as google } from "./assets/google.png";
+export { default as habitual } from "./assets/habitual.png";
+export { default as home } from "./assets/home.png";
+export { default as main } from "./assets/main.png";
+export { default as orders } from "./assets/orders.png";
+export { default as product } from "./assets/product.png";
+export { default as profile } from "./assets/profile.png";
